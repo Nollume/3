b29a3a0aa9de59f6e78f3903529a984d6375897b
nollume <marek.dorotovic@gmail.com>
2022-03-06 14:16:46 +0100

    pridal som súbory

---
+++ js/script.js
@@ -0,0 +1,150 @@
+(function($){
+
+var gallery = $('.gallery');
+    overlay = $('<div/>', {id:'overlay'}),
+    loading = $('.load');
+
+    overlay.appendTo('body').hide();
+    loading.hide();
+
+    gallery.on('click','a', function(e){
+        e.preventDefault();
+        
+        var href = $(this).attr('href'),
+            image = $('<img>',{src:href,alt:'obrazok'});
+
+            loading.show();
+
+
+
+        image.on('load', function() {
+               overlay.html(image).show();
+
+               loading.hide();
+
+            });
+    });
+
+    overlay.on('click', function(){
+
+        overlay.hide();
+    });
+
+    $(document).on('keyup', function(event){
+
+        if( event.which === 27) overlay.hide();
+    });
+
+
+    
+    var   galleries = $('.gallery-set'),
+          paginations = $('.pagination'),
+          loadIcon = $('<div class="loadingIcon"> <img src="load/_21_loading-gif-transparent-background_Free-Content-Discovery-Influencer-Marketing-Tool-Buzzsumo-.gif" alt="loading"></div>');
+
+          loadIcon.appendTo(gallery).hide();
+          
+
+$(window).on('scroll', function(){
+
+    if(loadIcon.is(':visible') )  return;
+
+    var data = paginations.data('bottom-page'),
+        id = '#' + data.slice(0,-5);
+    
+  
+    if($(window).scrollTop() + $(window).height() >= $(document).height()) 
+    {
+        console.log(id);
+        
+        if (gallery.find(id).length) {
+            loadIcon.hide('fast');
+            return ;
+        }
+        else{
+            loadNewGallery(data);   
+        }
+
+    };
+
+    function loadNewGallery(data){
+
+        loadIcon.show('fast');
+
+            var request = $.ajax({
+                url:data
+            }).promise();
+
+            request.done(function(data){
+
+                var newGallery = $(data).find('.gallery-set');
+
+                newGallery.appendTo(gallery);
+            });
+
+            request.fail(function(){
+                alert('nepodarilo sa');
+            });
+            request.always(function(){
+                loadIcon.hide();
+            });
+
+
+
+    };
+    
+          
+        
+});
+
+
+
+
+    // paginations.on('click', function(event){
+        
+    //          event.preventDefault();
+
+    //     var a = $(this),
+    //         li = a.parent();
+
+    //         if(li.is('.active') || loading.is(':visible') )  return;
+
+    //         var href = a.attr('href'),
+    //             currentGallery = gallery.find('.gallery-set');
+
+    //             li.addClass('active').siblings('li').removeClass('active');
+
+    //         var id = '#' + href.slice(0,-5);
+
+    //             if (gallery.find(id).length) {
+    //                 currentGallery.hide();
+    //                 $(id).show();
+    //             }
+    //             else {
+    //                 loadNewGallery(href);
+    //             }
+
+    //             function loadNewGallery(href){
+    //                 currentGallery.hide();
+    //                 loading.show();
+                
+    //             var request = $.ajax({
+    //                 url:href
+    //             });
+    //             request.done(function(data){
+    //                 var newGallery = $(data).find('.gallery-set');
+    //                 newGallery.hide().appendTo(gallery).show();
+    //                 currentGallery.hide();
+    //             });
+    //             request.fail(function(){
+    //                 alert('nepodarilo sa');
+    //             });
+    //             request.always(function(){
+    //                 loading.hide();
+    //             });
+    //         }
+    // });
+
+
+    
+
+}(jQuery));
